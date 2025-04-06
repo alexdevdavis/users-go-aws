270974aa5cc13885530c6af0116551352aac26d8
alexdevdavis <alexdevdavis@gmail.com>
2025-04-06 21:13:14 +0100

    feat: implement password hashing

--- lambda/types/types.go
+++ lambda/types/types.go
@@ -1,6 +1,31 @@
 package types
 
+import (
+	"golang.org/x/crypto/bcrypt"
+)
+
 type RegisterUser struct {
 	Username string `json:"username"`
 	Password string `json:"password"`
+}
+
+type User struct {
+	Username     string `json:"username"`
+	PasswordHash string `json:"password"`
+}
+
+func NewUser(ru RegisterUser) (User, error) {
+	hp, err := bcrypt.GenerateFromPassword([]byte(ru.Password), 10)
+	if err != nil {
+		return User{}, err
+	}
+	return User{
+		Username:     ru.Username,
+		PasswordHash: string(hp),
+	}, nil
+}
+
+func ValidateUser(hp, pp string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hp), []byte(pp))
+	return err == nil
 }
